f909e430bbe7ae747d4ea74cbea6411ec8b5c1eb
Frederick Akalin <akalin@gmail.com>
2016-08-19 17:45:06 -0700

    Add bare-bones ComplexFormula class

---
+++ complex_formula.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// f should be a function that takes an array of arrays of Complex
+// objects and returns an array of complex objects. If subformulas is
+// falsy or empty, then when update is called, f will be passed an
+// array of a single array containing the arguments of update
+// converted to Complex objects. Otherwise, f will be passed the array
+// of subresults from calling the subformulas with the arguments of
+// update.
+function ComplexFormula(f, subformulas) {
+  this._f = f;
+  this._subformulas = subformulas || [];
+}
+
+// This function takes in any number of objects and returns an array
+// of Complex objects.
+ComplexFormula.prototype.update = function() {
+  // Use a for loop as anything more clever prevents this use of
+  // arguments from being optimized out.
+  var inputs = [];
+  for (var i = 0; i < arguments.length; ++i) {
+    inputs.push(Complex.from(arguments[i]));
+  };
+  if (this._subformulas.length === 0) {
+    return this._f([inputs]);
+  }
+  var subresults = this._subformulas.map(function(subformula) {
+    return subformula.update.apply(subformula, inputs);
+  });
+  return this._f(subresults);
+};
---
+++ complex_formula_tests.js
@@ -0,0 +1,33 @@
+describe('complex formula', function() {
+  it('construction with no subformulas', function() {
+    var f = new ComplexFormula(function(subresults) {
+      return subresults[0].map(function(z) { return z.pow(2); });
+    });
+    var z = new Complex(3.1, 2.5);
+
+    var results = f.update(z, z.conj(), 5);
+    expect(results).toEqual([ z.pow(2), z.conj().pow(2), Complex.from(25) ]);
+  });
+
+  it('construction with subformulas', function() {
+    var f1 = new ComplexFormula(function(subresults) {
+      return subresults[0].map(function(z) { return z.pow(2); });
+    });
+    var f2 = new ComplexFormula(function(subresults) {
+      return subresults[0].map(function(z) { return z.pow(3); });
+    });
+    var g = new ComplexFormula(function(subresults) {
+      return subresults[0].map(function(_, i) {
+        return subresults[0][i].plus(subresults[1][i]);
+      });
+    }, [ f1, f2 ]);
+    var z = new Complex(3.1, 2.5);
+
+    var results = g.update(z, z.conj(), 2);
+    expect(results).toEqual([
+      z.pow(2).plus(z.pow(3)),
+      z.conj().pow(2).plus(z.conj().pow(3)),
+      Complex.from(12)
+    ]);
+  });
+});
